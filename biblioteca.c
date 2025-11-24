8db8df830db6a782307a3c5aeea7a6b6cecd5bb0
Atirf007 <cl205153@g.unicamp.br>
2025-11-24 19:53:51 -0300

    All functions now open 'biblioteca.txt'

--- biblioteca.c
+++ biblioteca.c
@@ -1,25 +1,26 @@
 #include "biblioteca.h"
 
 void cadastrar_livro(Livro acervo[], int *total_livros){
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
 void listar_livros(Livro acervo[], int total_livros){
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
 void emprestar_livro(Livro acervo[], int total_livros){
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
 void devolver_livro(Livro acervo[], int total_livros){
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
 void salvar_biblioteca(Livro acervo[], int total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
-
 
 void carregar_biblioteca(Livro acervo[], int *total_livros){
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
+
