e81d9867da9bfa32e0c340a9cb6fe3ec35a30ec0
Atirf007 <cl205153@g.unicamp.br>
2025-11-26 17:34:49 -0300

    Adiciona sistema de empréstimo de livros e remove o comando de escrever num arquivo da função 'cadastrar_livros'

--- biblioteca.c
+++ biblioteca.c
@@ -5,7 +5,10 @@
 void cadastrar_livro(Livro acervo[], int *totaLivros){
   
   printf("\n============= Cadastro de livros =============\n\n");
-  
+  if(totalLivros > MAX_LIVROS){
+    printf("Erro! Número máximo de livros cadastrados\n")
+  }
+  else{
   printf("Título: ");
   scanf(" %99[^\n]", acervo[*totalLivros].titulo);
   
@@ -15,21 +18,45 @@ void cadastrar_livro(Livro acervo[], int *totaLivros){
   acervo[*totalLivros].status = 0;
   
   printf("Livro cadastrado!\n");
-
-  FILE *ptrArquivo = fopen("biblioteca.txt", "a");
-  // 'a' adiciona sem apagar o conteúdo
-  fprintf(ptrArquivo, "%d - %s/%s/%d\n", *totalLivros +1, acervo[*totalLivros].titulo, acervo[*totalLivros].autor, acervo[*totalLivros].status);
-  fclose(ptrArquivo);
-  
     (*totalLivros)++;
+  }
 }
 
 void listar_livros(Livro acervo[], int total_livros){
   FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-void emprestar_livro(Livro acervo[], int total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+void emprestar_livro(Livro acervo[], int totalLivros){
+  int livroEncontrado = 1;
+
+  printf("\n============= Empréstimo de livros =============\n\n");
+  if (totalLivros == 0){
+    printf("Erro! Nenhum livro cadastrado\n");
+  }
+  else{
+    char tituloBusca[100];
+  
+  printf("Título: ");
+  scanf(" %99[^\n]", tituloBusca);
+  
+  for (int i = 0; i < totalLivros; i++){
+    
+  if (strcasecmp(acervo[i].titulo, tituloBusca) == 0){
+    livroEncontrado = 0;
+    
+    if(acervo[i].status == 0){
+      acervo[i].status = 1;
+      printf("Empréstimo de %s feito com sucesso!\n", acervo[i].titulo);
+    }
+    else{
+      printf("Erro! %s Já foi emprestado.", acervo[i].titulo);
+    }
+  }
+  }
+  if(livroEncontrado == 1){
+    printf("Erro! Livro não encontrado.\n")
+  }
+  }
 }
 
 void devolver_livro(Livro acervo[], int total_livros){
