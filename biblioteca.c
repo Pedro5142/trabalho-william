83c390c5aa492f02e0338b08f453a96b6502fe17
Atirf007 <cl205153@g.unicamp.br>
2025-11-26 15:28:46 -0300

    Mudanças no layout do cadastro de livros

--- biblioteca.c
+++ biblioteca.c
@@ -2,7 +2,10 @@
 #include <locale.h>
 #include "biblioteca.h"
 
-void cadastrar_livro(Livro acervo[], int *totalLivros) {
+void cadastrar_livro(Livro acervo[], int *totaLivros){
+  
+  printf("\n============= Cadastro de livros =============\n\n");
+  
   printf("Título: ");
   scanf(" %99[^\n]", acervo[*totalLivros].titulo);
   
@@ -15,34 +18,37 @@ void cadastrar_livro(Livro acervo[], int *totalLivros) {
 
   FILE *ptrArquivo = fopen("biblioteca.txt", "a");
   // 'a' adiciona sem apagar o conteúdo
-  fprintf(ptrArquivo, "%s/%s/%d\n", acervo[*totalLivros].titulo, acervo[*totalLivros].autor, acervo[*totalLivros].status);
+  fprintf(ptrArquivo, "%d - %s/%s/%d\n", *totalLivros +1, acervo[*totalLivros].titulo, acervo[*totalLivros].autor, acervo[*totalLivros].status);
   fclose(ptrArquivo);
   
     (*totalLivros)++;
 }
 
-void listar_livros(Livro acervo[], int totalLivros) {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+void listar_livros(Livro acervo[], int total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-void emprestar_livro(Livro acervo[], int totalLivros) {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+void emprestar_livro(Livro acervo[], int total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-void devolver_livro(Livro acervo[], int totalLivros) {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+void devolver_livro(Livro acervo[], int total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-void salvar_biblioteca(Livro acervo[], int totalLivros) {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+void salvar_biblioteca(Livro acervo[], int total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-void carregar_biblioteca(Livro acervo[], int *totalLivros) {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+void carregar_biblioteca(Livro acervo[], int *total_livros){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 }
 
-int main() {
-    FILE *ptrArquivo = fopen("biblioteca.txt", "w+");
+int main(){
+  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
 
-    return 0;
+
+
+
+return 0;
 }
