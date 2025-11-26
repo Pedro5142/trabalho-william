3d395983d209809e57e8eafc3a26d5bcf91fb3c7
Pedro5142 <cl205156@g.unicamp.br>
2025-11-26 22:22:11 +0000

    Adicionado listar livros e carregar biblioteca

--- biblioteca.c
+++ biblioteca.c
@@ -3,34 +3,55 @@
 #include "biblioteca.h"
 
 void cadastrar_livro(Livro acervo[], int *total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+  FILE *ptrarquivo = fopen("biblioteca.txt", "a");
 }
 
 void listar_livros(Livro acervo[], int total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+  for(int i = 0; i < total_livros; i++){
+    printf("--------Livro %d--------\n", i + 1);
+    printf("Título: %s\nAutor: %s\nStatus: ", acervo[i].titulo, acervo[i].autor);
+    if(acervo[i].status == 0){
+      printf("Disponível\n\n");
+    }
+    else{
+      printf("Emprestado\n\n");
+    }
+  }
 }
 
 void emprestar_livro(Livro acervo[], int total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+  
 }
 
 void devolver_livro(Livro acervo[], int total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+  
 }
 
 void salvar_biblioteca(Livro acervo[], int total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
+  FILE *ptrarquivo = fopen("biblioteca.txt", "a");
 }
 
 void carregar_biblioteca(Livro acervo[], int *total_livros){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
-}
-
-int main(){
-  FILE *ptrarquivo = fopen("biblioteca.txt", "w+");
-
-
+  FILE *ptrarquivo = fopen("biblioteca.txt", "rb");
   
+  printf("%d", *total_livros);
 
-return 0;
+  if(ptrarquivo != NULL){
+    
+    int i = 0;
+    int j = 0;
+
+    while((j = fread(&acervo[i], sizeof(Livro), 1, ptrarquivo)) == 1){
+      printf("%d ", j);
+      *total_livros = *total_livros + 1;
+      i++;
+    }
+
+    printf("Biblioteca carregada. Total de livros: %d\n", *total_livros);
+  }
+  else{
+    printf("Erro ao abrir arquivo\n");
+  }
+
+  fclose(ptrarquivo);
 }
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
 #include "biblioteca.h"
 
@@ -10,27 +11,46 @@ int main(){
 
     Livro acervo[MAX_LIVROS];
 
-    int menu = -1, opcao;
+    int menu = -1, opcao, total_livros = 0;
+
+    FILE *ptrarquivo2 = fopen("biblioteca.txt", "ab");
+
+    strcpy(acervo[0].titulo, "titulo1");
+    strcpy(acervo[0].autor, "autor1");
+    acervo[0].status = 1;
+
+    strcpy(acervo[1].titulo, "titulo2");
+    strcpy(acervo[1].autor, "autor2");
+    acervo[1].status = 0;
+
+    fwrite(&acervo[0], sizeof(acervo[0]), 1, ptrarquivo2);
+    fwrite(&acervo[1], sizeof(acervo[1]), 1, ptrarquivo2);
+
+    for(int i = 0; i < 2; i++){
+        printf("%s, %s, %d\n", acervo[i].titulo, acervo[i].autor, acervo[0].status);
+    }
+
+    fclose(ptrarquivo2);
 
     while(menu != 0){
         printf("=============Sistema Biblioteca=============\n\n");
-        printf("O que deseja fazer?\n");
-        printf("1 - Carregar Biblioteca\n2 - Listar Livros\n3 - Emprestar livro\n4 - Devolver livro\n5 - Cadastrar Livro\n6 - Salvar biblioteca\n0 - Sair\n\n");
+        printf("O que deseja fazer?\n\n");
+        printf("1 - Carregar Biblioteca\n2 - Listar Livros\n3 - Emprestar livro\n4 - Devolver livro\n5 - Cadastrar Livro\n6 - Salvar biblioteca\n0 - Sair\n");
         scanf("%d", &menu);
 
         if(menu == 1){
-            //carregar_biblioteca();
+            carregar_biblioteca(acervo, &total_livros);
             printf("\n");
         }
 
         if(menu == 2){
-            //listar_livros();
+            listar_livros(acervo, total_livros);
             printf("\n");
         }
 
         while(menu == 3){
             
-            //emprestar_livro();
+            emprestar_livro();
 
             printf("\nDeseja emprestar outro livro? (1 = sim / 2 = não) ");
             scanf("%d", &opcao);
@@ -43,7 +63,7 @@ int main(){
         
         while(menu == 4){
             
-            //devolver_livro();
+            devolver_livro();
             
             printf("\nDeseja devolver outro livro? (1 = sim / 2 = não) ");
             scanf("%d", &opcao);
@@ -56,7 +76,7 @@ int main(){
         
         while(menu == 5){
 
-            //cadastrar_livro();
+            cadastrar_livro();
             
             printf("\nDeseja cadastrar outro livro? (1 = sim / 2 = não) ");
             scanf("%d", &opcao);
@@ -68,7 +88,7 @@ int main(){
         }
         
         if(menu == 6){
-            //salvar_biblioteca();
+            salvar_biblioteca();
             printf("\n");
         }
     }
